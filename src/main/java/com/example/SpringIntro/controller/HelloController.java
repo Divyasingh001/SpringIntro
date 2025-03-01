6cbe26098d19eb5eecc943866036a1fc1b5e33fb
Divyasingh001 <divyashunk2003@gmail.com>
2025-03-01 16:09:54 +0530

    Added

--- src/main/java/com/example/SpringIntro/controller/HelloController.java
+++ src/main/java/com/example/SpringIntro/controller/HelloController.java
@@ -10,19 +10,27 @@ public class HelloController {
         return "Hello from BridgeLabz";
     }
    //used request  mapping to get the query in the form of name and display it
+//    http://localhost:8080/query?name=Divya
     @RequestMapping(value="/query")
     public String queryController(@RequestParam(value="name") String name){
         return "Hello "+name+" !";
     }
 
     //used params to take input and display it
+//    http://localhost:8080/param/Divya
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name){
         return "Hello "+name +"! ";
     }
-
+//    http://localhost:8080/post
     @PostMapping("/post")
     public String sayHello(@RequestBody User user){
         return "Hello"+user.getFirstName() +" "+ user.getLastName()+" !";
+    }
+//    http://localhost:8080/put/Divya?LastName=Singh
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable  String firstName,
+                           @RequestParam(value = "lastName") String lastName){
+        return "Hello "+firstName+" "+lastName+" !";
     }
 }
