a7af6d20d5ce18f8dce2cd533d7a46ec04e613ae
Divyasingh001 <divyashunk2003@gmail.com>
2025-03-01 15:30:57 +0530

    Added

--- src/main/java/com/example/SpringIntro/controller/HelloController.java
+++ src/main/java/com/example/SpringIntro/controller/HelloController.java
@@ -8,9 +8,15 @@ public class HelloController {
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
-
+   //used request  mapping to get the query in the form of name and display it
     @RequestMapping(value="/query")
     public String queryController(@RequestParam(value="name") String name){
         return "Hello "+name+" !";
+    }
+
+    //used params to take input and display it
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name){
+        return "Hello "+name +"! ";
     }
 }
